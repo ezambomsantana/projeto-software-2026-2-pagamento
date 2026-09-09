19389d260c246e313322e8a64fe7cc8805e9a321
Eduardo Santana <ezambomsantana@gmail.com>
2026-09-09 10:30:49 -0300

    add tests

--- src/test/java/br/insper/pagamento/service/PagamentoServiceTests.java
+++ src/test/java/br/insper/pagamento/service/PagamentoServiceTests.java
@@ -3,6 +3,7 @@ package br.insper.pagamento.service;
 import br.insper.pagamento.dto.PagamentoDto;
 import br.insper.pagamento.entity.Pagamento;
 import br.insper.pagamento.entity.TipoPagamento;
+import br.insper.pagamento.processor.Processador;
 import br.insper.pagamento.repository.PagamentoRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -16,6 +17,8 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 @ExtendWith(MockitoExtension.class)
 public class PagamentoServiceTests {
@@ -25,6 +28,98 @@ public class PagamentoServiceTests {
 
     @Mock
     private PagamentoRepository pagamentoRepository;
+
+    @Mock
+    private Map<String, Processador> processadores;
+
+    @Mock
+    private Processador processador;
+
+
+    @Test
+    public void test_shouldReturnPagamentoWhenCallObterPorId() {
+
+        PagamentoDto dto = new PagamentoDto();
+        dto.setTipo(TipoPagamento.PIX);
+        dto.setValor(new BigDecimal(1000));
+        dto.setDataCompra(LocalDate.now());
+        dto.setChaveOrigem("123");
+        dto.setChaveDestino("234");
+
+        Pagamento pagamento = Pagamento.fromDto(dto);
+
+        // mocks
+        Mockito.when(pagamentoRepository.findById(1L))
+                .thenReturn(Optional.of(pagamento));
+
+        // chamada
+        Optional<Pagamento> op = pagamentoService.obterPorId(1L);
+
+        // asserts
+        Assertions.assertTrue(op.isPresent());
+        Assertions.assertEquals("543", op.get().getChaveOrigem());
+        Assertions.assertEquals("234", op.get().getChaveDestino());
+        Assertions.assertEquals(TipoPagamento.PIX, op.get().getTipo());
+    }
+
+    @Test
+    public void test_shouldReturnSuccessWhenCallProcessador() {
+
+        PagamentoDto dto = new PagamentoDto();
+        dto.setTipo(TipoPagamento.PIX);
+        dto.setValor(new BigDecimal(1000));
+        dto.setDataCompra(LocalDate.now());
+        dto.setChaveOrigem("123");
+        dto.setChaveDestino("234");
+
+        Pagamento pagamento = Pagamento.fromDto(dto);
+
+        // mocks
+        Mockito.when(pagamentoRepository.findById(1L))
+                .thenReturn(Optional.of(pagamento));
+        Mockito.when(processadores.get(Mockito.any()))
+                .thenReturn(processador);
+        Mockito.when(processador.processar(Mockito.any()))
+                .thenReturn(true);
+        Mockito.when(pagamentoRepository.save(Mockito.any()))
+                .thenReturn(pagamento);
+
+        // chamada
+        boolean response = pagamentoService.processar(1L);
+
+        // asserts
+        Assertions.assertTrue(response);
+    }
+
+
+    @Test
+    public void test_shouldReturnFalseWhenCallProcessador() {
+
+        PagamentoDto dto = new PagamentoDto();
+        dto.setTipo(TipoPagamento.PIX);
+        dto.setValor(new BigDecimal(1000));
+        dto.setDataCompra(LocalDate.now());
+        dto.setChaveOrigem("123");
+        dto.setChaveDestino("234");
+
+        Pagamento pagamento = Pagamento.fromDto(dto);
+
+        // mocks
+        Mockito.when(pagamentoRepository.findById(1L))
+                .thenReturn(Optional.of(pagamento));
+        Mockito.when(processadores.get(Mockito.any()))
+                .thenReturn(processador);
+        Mockito.when(processador.processar(Mockito.any()))
+                .thenReturn(false);
+        Mockito.when(pagamentoRepository.save(Mockito.any()))
+                .thenReturn(pagamento);
+
+        // chamada
+        boolean response = pagamentoService.processar(1L);
+
+        // asserts
+        Assertions.assertFalse(response);
+    }
 
 
     @Test
